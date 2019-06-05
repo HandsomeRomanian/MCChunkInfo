50ccc91c561968dee1970f74f6a05e8d8106af41
HandsomeRomanian <mat4school@gmail.com>
2019-06-04 23:45:12 -0400

    Changed Form Title

--- MinecraftChunkPriorityCalculator/Form1.Designer.cs
+++ MinecraftChunkPriorityCalculator/Form1.Designer.cs
@@ -28,13 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbChunkX = new System.Windows.Forms.TextBox();
+            this.tbChunkY = new System.Windows.Forms.TextBox();
+            this.tbPlayerY = new System.Windows.Forms.TextBox();
+            this.tbPlayerX = new System.Windows.Forms.TextBox();
+            this.lblChunkX = new System.Windows.Forms.Label();
+            this.lblChunkY = new System.Windows.Forms.Label();
+            this.lblPlayerY = new System.Windows.Forms.Label();
+            this.lblPlayerX = new System.Windows.Forms.Label();
+            this.lblPriorityTitle = new System.Windows.Forms.Label();
+            this.lblPriority = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbChunkX
+            // 
+            this.tbChunkX.Location = new System.Drawing.Point(108, 34);
+            this.tbChunkX.Name = "tbChunkX";
+            this.tbChunkX.Size = new System.Drawing.Size(100, 20);
+            this.tbChunkX.TabIndex = 0;
+            this.tbChunkX.TextChanged += new System.EventHandler(this.tbChunkX_TextChanged);
+            // 
+            // tbChunkY
+            // 
+            this.tbChunkY.Location = new System.Drawing.Point(108, 60);
+            this.tbChunkY.Name = "tbChunkY";
+            this.tbChunkY.Size = new System.Drawing.Size(100, 20);
+            this.tbChunkY.TabIndex = 1;
+            this.tbChunkY.TextChanged += new System.EventHandler(this.tbChunkY_TextChanged);
+            // 
+            // tbPlayerY
+            // 
+            this.tbPlayerY.Location = new System.Drawing.Point(108, 198);
+            this.tbPlayerY.Name = "tbPlayerY";
+            this.tbPlayerY.Size = new System.Drawing.Size(100, 20);
+            this.tbPlayerY.TabIndex = 3;
+            this.tbPlayerY.TextChanged += new System.EventHandler(this.tbPlayerY_TextChanged);
+            // 
+            // tbPlayerX
+            // 
+            this.tbPlayerX.Location = new System.Drawing.Point(108, 172);
+            this.tbPlayerX.Name = "tbPlayerX";
+            this.tbPlayerX.Size = new System.Drawing.Size(100, 20);
+            this.tbPlayerX.TabIndex = 2;
+            this.tbPlayerX.TextChanged += new System.EventHandler(this.tbPlayerX_TextChanged);
+            // 
+            // lblChunkX
+            // 
+            this.lblChunkX.AutoSize = true;
+            this.lblChunkX.Location = new System.Drawing.Point(38, 40);
+            this.lblChunkX.Name = "lblChunkX";
+            this.lblChunkX.Size = new System.Drawing.Size(48, 13);
+            this.lblChunkX.TabIndex = 4;
+            this.lblChunkX.Text = "Chunk X";
+            // 
+            // lblChunkY
+            // 
+            this.lblChunkY.AutoSize = true;
+            this.lblChunkY.Location = new System.Drawing.Point(38, 67);
+            this.lblChunkY.Name = "lblChunkY";
+            this.lblChunkY.Size = new System.Drawing.Size(48, 13);
+            this.lblChunkY.TabIndex = 5;
+            this.lblChunkY.Text = "Chunk Y";
+            // 
+            // lblPlayerY
+            // 
+            this.lblPlayerY.AutoSize = true;
+            this.lblPlayerY.Location = new System.Drawing.Point(37, 202);
+            this.lblPlayerY.Name = "lblPlayerY";
+            this.lblPlayerY.Size = new System.Drawing.Size(46, 13);
+            this.lblPlayerY.TabIndex = 7;
+            this.lblPlayerY.Text = "Player Y";
+            // 
+            // lblPlayerX
+            // 
+            this.lblPlayerX.AutoSize = true;
+            this.lblPlayerX.Location = new System.Drawing.Point(37, 175);
+            this.lblPlayerX.Name = "lblPlayerX";
+            this.lblPlayerX.Size = new System.Drawing.Size(46, 13);
+            this.lblPlayerX.TabIndex = 6;
+            this.lblPlayerX.Text = "Player X";
+            // 
+            // lblPriorityTitle
+            // 
+            this.lblPriorityTitle.AutoSize = true;
+            this.lblPriorityTitle.Location = new System.Drawing.Point(273, 41);
+            this.lblPriorityTitle.Name = "lblPriorityTitle";
+            this.lblPriorityTitle.Size = new System.Drawing.Size(38, 13);
+            this.lblPriorityTitle.TabIndex = 8;
+            this.lblPriorityTitle.Text = "Priority";
+            // 
+            // lblPriority
+            // 
+            this.lblPriority.AutoSize = true;
+            this.lblPriority.Location = new System.Drawing.Point(329, 41);
+            this.lblPriority.Name = "lblPriority";
+            this.lblPriority.Size = new System.Drawing.Size(13, 13);
+            this.lblPriority.TabIndex = 9;
+            this.lblPriority.Text = "0";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(40, 263);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(168, 23);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "Manualy Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(316, 79);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(51, 13);
+            this.lblFile.TabIndex = 12;
+            this.lblFile.Text = "r.0.0.mca";
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(273, 79);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(26, 13);
+            this.lblFileName.TabIndex = 11;
+            this.lblFileName.Text = "File:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(391, 333);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblPriority);
+            this.Controls.Add(this.lblPriorityTitle);
+            this.Controls.Add(this.lblPlayerY);
+            this.Controls.Add(this.lblPlayerX);
+            this.Controls.Add(this.lblChunkY);
+            this.Controls.Add(this.lblChunkX);
+            this.Controls.Add(this.tbPlayerY);
+            this.Controls.Add(this.tbPlayerX);
+            this.Controls.Add(this.tbChunkY);
+            this.Controls.Add(this.tbChunkX);
+            this.Name = "Form1";
+            this.Text = "Minecraft Chunk ToolKit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbChunkX;
+        private System.Windows.Forms.TextBox tbChunkY;
+        private System.Windows.Forms.TextBox tbPlayerY;
+        private System.Windows.Forms.TextBox tbPlayerX;
+        private System.Windows.Forms.Label lblChunkX;
+        private System.Windows.Forms.Label lblChunkY;
+        private System.Windows.Forms.Label lblPlayerY;
+        private System.Windows.Forms.Label lblPlayerX;
+        private System.Windows.Forms.Label lblPriorityTitle;
+        private System.Windows.Forms.Label lblPriority;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.Label lblFileName;
     }
 }
 
--- MinecraftChunkPriorityCalculator/Form1.cs
+++ MinecraftChunkPriorityCalculator/Form1.cs
@@ -15,6 +15,113 @@ namespace MinecraftChunkPriorityCalculator
         public Form1()
         {
             InitializeComponent();
+            tbPlayerX.Text = "0";
+            tbPlayerY.Text = "0";
+            tbChunkX.Text = "0";
+            tbChunkY.Text = "0";
+        }
+
+        private void tbChunkX_TextChanged(object sender, EventArgs e)
+        {
+            update();
+        }
+
+        private void tbChunkY_TextChanged(object sender, EventArgs e)
+        {
+            update();
+        }
+
+        private void tbPlayerX_TextChanged(object sender, EventArgs e)
+        {
+            tbChunkX.Text = playerToChunk(tbPlayerX.Text);
+        }
+
+        private void tbPlayerY_TextChanged(object sender, EventArgs e)
+        {
+            tbChunkY.Text = playerToChunk(tbPlayerY.Text);
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            update();
+        }
+
+        private void update()
+        {
+            calculatePriority();
+            getFileName();
+        }
+
+        /// <summary>
+        /// Signed binaries:
+        /// first bit is the sign 0 for positive, 1 for negative.
+        /// next 8 are for exponent
+        /// last 23 are mantis
+        /// 0 00000000 00000000000000000000000
+        /// </summary>
+        private void calculatePriority()
+        {
+
+            if (tbChunkX.Text != null && tbChunkX.Text != "" && tbChunkX.Text != "-" && tbChunkY.Text != null && tbChunkY.Text != "" && tbChunkY.Text != "-")
+            {
+                
+                String x = Convert.ToString(int.Parse(tbChunkX.Text), 2);
+                String y = Convert.ToString(int.Parse(tbChunkY.Text), 2);
+                string xor1 = xorStrings(x, y).PadLeft(32,'0');
+                string[] table = splitInHalf(xor1);
+                string a = table[0];
+                string b = table[1];
+                string xor2 = xorStrings(a, b);
+
+
+                
+                int priority = Convert.ToInt32(xor2, 2);
+                lblPriority.Text = priority.ToString();
+
+            }
+        }
+        private string xorStrings(string a, string b)
+        {
+            string output = "";
+            int len = Math.Max(a.Length, b.Length);
+            a = a.PadLeft(len, '0');
+            b = b.PadLeft(len, '0');
+
+            for (int i = 0; i < len; i++)
+            {
+                output += a[i] == b[i] ? '0' : '1';
+            }
+            return output;
+        }
+        private string[] splitInHalf(string input)
+        {
+            string[] output = new string[2];
+            for (int i = 0; i < input.Length/2; i++)
+            {
+                output[0] += input[i];
+                output[1] += input[input.Length/2 + i];
+            }
+            return output;
+        }
+        private string playerToChunk(string coords)
+        {
+            if (coords == null || coords == "" || coords == "-")
+                return "0";
+            string output ="";
+            int value = int.Parse(coords);
+            output += (value / 16).ToString();
+            return output;
+        }
+
+        private void getFileName()
+        {
+            int x = 0;
+            int y = 0;
+            if (tbChunkX.Text != "")
+                x = int.Parse(tbChunkX.Text);
+            if (tbChunkY.Text != "")
+                y = int.Parse(tbChunkY.Text);
+            lblFile.Text = "r." + (x / 512) + "." + (y / 512) + ".mca";
         }
     }
 }
